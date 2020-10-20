dc10301faede2395c72e38628e8e3790c470e536
Laurent Vetterhoeffer <vetterh1@yahoo.fr>
2020-10-20 12:07:20 +0200

    removed token restriction on GET custom size image

--- src/api/customSizeImage/index.js
+++ src/api/customSizeImage/index.js
@@ -1,38 +1,22 @@
 import { Router } from 'express'
 import { token } from '../../services/passport'
 import { GetCustomSizeImage, DeleteRelatedCustomSizeImages } from './controller'
-// import { foldersPaths } from '../../config'
-// import path from 'path';
-
-
-
-
-
-  // // Serve static assets (pictures,...)
-  // const folderStaticAbsolute = path.join(__dirname, foldersPaths.relativePaths.fromExpress, foldersPaths.static);
-  // if (env === 'production' || env === 'development')
-  //   console.info(`Public (static) files should be here: ${folderStaticAbsolute}`);
-  // // app.use('/public', express.static(folderStaticAbsolute), serveIndex(folderStaticAbsolute, {'icons': true}));
-  // app.use('/static', express.static(folderStaticAbsolute));
-
-
 
 const router = new Router()
 
 /**
- * @api {get} /custom-size-image/:type/:filename Retrieve file
+ * @api {get} /custom-size-image/:filename?MANDATORY_PARAMETERS Retrieve file
  * @apiName GetCustomSizeImage
  * @apiGroup CustomSizeImages
- * @apiPermission user
+ * @apiPermission none
  * @apiParam {String} access_token user access token.
- * @apiParam {String} :filename of the picture, followed by query parameters in option (ex: ?type=item&width=230&height=230)
+ * @apiParam {String} :filename of the picture, followed by MANDATORY query parameters (ex: ?type=item&width=230&height=230)
  * @apiSuccess {Object} file.
  * @apiError {Object} 400 Some parameters may contain invalid values.
  * @apiError 404 Item not found.
- * @apiError 401 user access only.
  */
 router.get('/:filename',
-  token({ required: true }),
+  // token({ required: true }),
   GetCustomSizeImage)
 
 
